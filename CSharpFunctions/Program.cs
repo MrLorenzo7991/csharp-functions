8b726ed7d729f143933b285d7100a73642a94d1d
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-05 14:28:42 +0200

    Completata Function StampaArray

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿/*----FUNZIONI----*/
+
+int[] arrayDiProva = { 2, 4, 9, 13, 43, 99 };
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+StampaArray(arrayDiProva);
